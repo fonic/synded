fc076e88b29483792213f4276fd0046ecd06d184
Fonic <5cx5jbnmt@mozmail.com>
2025-10-08 10:06:03 +0200

    Add basic Person editor

---
+++ PersonRW/personrw.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define PA_OFFSET 32776    // Offset of People array in GAMExx.DAT
+#define PA_LENGTH 256      // Number of items in People array
+
+struct Person {            // Person struct (item of People array)
+	uint16_t  Child;
+	uint16_t  Parent;
+	uint16_t  Xpos;
+	uint16_t  Ypos;
+	uint16_t  Zpos;
+	uint8_t   Status;
+	uint8_t   Affect;
+	uint8_t   Unknown1;
+	uint8_t   Unknown2;
+	uint16_t  BaseFrame;
+	uint16_t  Frame;
+	uint16_t  OldFrame;
+	uint16_t  Life;
+	uint16_t  WhoShotMe;
+	uint8_t   Model;
+	uint8_t   State;
+	uint8_t   Angle;
+	uint8_t   ZAngle;
+	uint16_t  Unique;
+	uint8_t   Hugdistance1;
+	uint16_t  Hugdistance2;
+	uint16_t  Persuaded;
+	uint16_t  ChildHeld;
+	uint16_t  ParentHeld;
+	uint16_t  Command;
+	uint16_t  StartCommand;
+	uint16_t  Target;
+	uint16_t  Data;
+	uint16_t  GotoX;
+	uint16_t  GotoY;
+	uint16_t  GotoZ;
+	uint16_t  LastXpos;
+	uint16_t  LastYpos;
+	uint16_t  HugGotoZ;
+	uint16_t  ChildWeapon;
+	uint16_t  Equipment;
+	uint16_t  ParentRank;
+	uint16_t  RankPosition;
+	uint16_t  WeaponHolding;
+	uint8_t   WeaponInUse;
+
+	uint8_t   Level1Base;
+	uint8_t   Level1Balance;
+	uint8_t   Level1True;
+	uint8_t   Level1Fixed;
+	uint8_t   Level2Base;
+	uint8_t   Level2Balance;
+	uint8_t   Level2True;
+	uint8_t   Level2Fixed;
+	uint8_t   Level3Base;
+	uint8_t   Level3Balance;
+	uint8_t   Level3True;
+	uint8_t   Level3Fixed;
+
+	uint8_t   LevelRecovery;
+	uint8_t   Speed;
+	uint8_t   MaxSpeed;
+	uint8_t   BaseSpeed;
+
+	uint8_t   OldState;
+	uint8_t   NewState;
+	uint8_t   HugChangeDir;
+	uint8_t   HugStartAngle;
+	uint8_t   HitAngle;
+};
+
+
+int main(int argc, char *argv[]) {
+
+	// Check command line
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s GAMExx.DAT\n", argv[0]);
+		return 1;
+	}
+
+	// Create People array (256 Person structs)
+	struct Person people[PA_LENGTH];
+
+	// Open file specified on command line
+	char *file_name = argv[1];
+	printf("Opening file '%s'...\n", file_name);
+	FILE *file = fopen(file_name, "r+b"); // read/write binary mode
+	if (!file) {
+		fprintf(stderr, "Failed to open file '%s': %s\n", file_name, strerror(errno));
+		return 1;
+	}
+
+	// Seek to offset of People array in file
+	printf("Seeking in file '%s'...\n", file_name);
+	if (fseek(file, PA_OFFSET, SEEK_SET) != 0 ) {
+		fprintf(stderr, "Failed to seek in file '%s': %s\n", file_name, strerror(errno));
+		return 1;
+	}
+
+	// Read data from file into array
+	printf("Reading from file '%s'...\n", file_name);
+	size_t read_count = fread(people, sizeof(struct Person), PA_LENGTH, file);
+	if (read_count != PA_LENGTH) {
+		fprintf(stderr, "Error reading from file '%s': %s\n", file_name, strerror(errno));
+		fclose(file);
+		return 1;
+	}
+
+
+	// Print fields of each Person in People array
+	printf("\n");
+	for (size_t i = 0; i < PA_LENGTH; ++i) {
+		struct Person* person = &people[i];
+		if ((person->Model == 0) && (person->Unique == 0))
+			break; // End of defined People reached
+		printf("Person %zu:\n", i+1);
+		printf("  Child: %u, Parent: %u\n", person->Child, person->Parent);
+		printf("  Xpos: %u, Ypos: %u, Zpos: %u\n", person->Xpos, person->Ypos, person->Zpos);
+		printf("  Status: %u, Affect: %u\n", person->Status, person->Affect);
+		printf("  Unknown1: %u, Unknown2: %u\n", person->Unknown1, person->Unknown2);
+		printf("  BaseFrame: %u, Frame: %u, OldFrame: %u\n", person->BaseFrame, person->Frame, person->OldFrame);
+		printf("  Life: %u\n", person->Life);
+		printf("  WhoShotMe: %u\n", person->WhoShotMe);
+		printf("  Model: %u, State: %u\n", person->Model, person->State);
+		printf("  Angle: %u, ZAngle: %u\n", person->Angle, person->ZAngle);
+		printf("  Unique: %u\n", person->Unique);
+		printf("  Hugdistance1: %u, Hugdistance2: %u\n", person->Hugdistance1, person->Hugdistance2);
+		printf("  Persuaded: %u\n", person->Persuaded);
+		printf("  ChildHeld: %u, ParentHeld: %u\n", person->ChildHeld, person->ParentHeld);
+		printf("  Command: %u, StartCommand: %u\n", person->Command, person->StartCommand);
+		printf("  Target: %u, Data: %u\n", person->Target, person->Data);
+		printf("  GotoX: %u, GotoY: %u, GotoZ: %u\n", person->GotoX, person->GotoY, person->GotoZ);
+		printf("  LastXpos: %u, LastYpos: %u, HugGotoZ: %u\n", person->LastXpos, person->LastYpos, person->HugGotoZ);
+		printf("  ChildWeapon: %u, Equipment: %u\n", person->ChildWeapon, person->Equipment);
+		printf("  ParentRank: %u, RankPosition: %u\n", person->ParentRank, person->RankPosition);
+		printf("  WeaponHolding: %u, WeaponInUse: %u\n", person->WeaponHolding, person->WeaponInUse);
+		printf("  Level1Base: %u, Level1Balance: %u, Level1True: %u, Level1Fixed: %u\n", person->Level1Base, person->Level1Balance, person->Level1True, person->Level1Fixed);
+		printf("  Level2Base: %u, Level2Balance: %u, Level2True: %u, Level2Fixed: %u\n", person->Level2Base, person->Level2Balance, person->Level2True, person->Level2Fixed);
+		printf("  Level3Base: %u, Level3Balance: %u, Level3True: %u, Level3Fixed: %u\n", person->Level3Base, person->Level3Balance, person->Level3True, person->Level3Fixed);
+		printf("  LevelRecovery: %u\n", person->LevelRecovery);
+		printf("  Speed: %u, MaxSpeed: %u, BaseSpeed: %u\n", person->Speed, person->MaxSpeed, person->BaseSpeed);
+		printf("  OldState: %u, NewState: %u\n", person->OldState, person->NewState);
+		printf("  HugChangeDir: %u, HugStartAngle: %u, HitAngle: %u\n", person->HugChangeDir, person->HugStartAngle, person->HitAngle);
+		printf("\n");
+	}
+
+
+	// Example modification: change health of first Person
+	//people[0].Life = 99;
+
+
+	// Seek to offset of People array in file
+	printf("Seeking in file '%s'...\n", file_name);
+	if (fseek(file, PA_OFFSET, SEEK_SET) != 0 ) {
+		fprintf(stderr, "Failed to seek in file '%s': %s\n", file_name, strerror(errno));
+		return 1;
+	}
+
+	// Write modified array to file
+	printf("Writing to file '%s'...\n", file_name);
+	size_t write_count = fwrite(people, sizeof(struct Person), PA_LENGTH, file);
+	if (write_count != PA_LENGTH) {
+		fprintf(stderr, "Error writing to file '%s': %s\n", file_name, strerror(errno));
+		fclose(file);
+		return 1;
+	}
+
+	// Close file and we're done
+	fclose(file);
+	printf("Successfully updated file '%s'.\n", file_name);
+	return 0;
+}
+
+// NOTE: Compile using:
+// gcc -o reader reader.c
